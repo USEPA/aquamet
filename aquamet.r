a3db89a344e6701ef4e6b85871b825f227015a05
Curt Seeliger <seeliger.curt@epa.gov>
2015-10-20 17:46:26 +0100

    Began modifying call interface for mets functions, beginning with nrsaBankMorphology

---
+++ aquamet.r
@@ -0,0 +1,33 @@
+# aquamet.r
+#
+# Source()s files and loads packages for aquamet.  Not to be included in aquamet
+# package.
+#
+
+require(foreach)
+require(Hmisc)
+#require(aquamet)
+require(RUnit)
+require(plyr)
+require(dplyr)
+require(reshape2)
+
+# Get aquamet function definitions
+if(substr(getwd(),1,1) == 'C') {
+    # local development
+    mainPath <- sprintf('C:/Users/%s/local/aquamet', Sys.getenv('USERNAME'))
+} else {
+    # shared space on L drive
+    mainPath <- getwd()
+}
+
+pathList <- c(sprintf("%s/%s", mainPath, 'R')
+             ,sprintf("%s/%s", mainPath, 'UnitTests/NLA Physical Habitat')
+             ,sprintf("%s/%s", mainPath, 'UnitTests/NRSA Physical Habitat')
+             ,sprintf("%s/%s", mainPath, 'UnitTests/SharedCode')
+             )
+for(path in pathList) {
+    srcList <- grep('^.+\\.[rR]$', list.files(path), value=TRUE)
+    for(src in srcList) 
+        source(sprintf("%s/%s", path, src))
+}
