89d7eda1addff48b3eb877aadcf01b5714ea6369
curt seeliger <seeliger.curt@epa.gov>
2024-02-21 12:12:52 -0800

    Changed title of test suites (NLA, NRSA, SharedCode)

--- unitTests/NLA_Physical_Habitat/testSuiteNLAMetrics.R
+++ unitTests/NLA_Physical_Habitat/testSuiteNLAMetrics.R
@@ -2,14 +2,19 @@
 #
 #  2/20/24 cws Added rngKind and rngNormalKind values to correct defaults as was
 #          recently done for other test suites.
+#  2/21/24 cws Specifying full path for output file. Improved title of test suite
 #
 
 testSuiteNLAMetrics <- function()
 # Define and run the test suite for the NLA physical habitat metrics functions
 # Results are saved to timestamped HTML file
 {
-  testSuite <- defineTestSuite("NLA metrics"
-                              ,dirs=aquametPathList
+  testsuiteTitle <- sprintf('Aquamet NLA metrics in %s and %s'
+                            ,ifelse(Sys.getenv('RSTUDIO') == '', 'RGui', 'RStudio')
+                            ,R.version.string
+                           )
+  testSuite <- defineTestSuite(testsuiteTitle
+                               ,dirs=aquametPathList
                               ,testFileRegexp="^nla.*\\.r$"
                               ,testFuncRegexp="^.+Test$"
                               ,rngKind = "Mersenne-Twister"
@@ -18,6 +23,6 @@ testSuiteNLAMetrics <- function()
 
   testResult <- runTestSuite(testSuite)
 
-  testResultFile <- "testResults_NLAMetrics.html"
+  testResultFile <- paste0(mainPath, "/testResults_NLAMetrics.html")
   printHTMLProtocol(testResult, fileName=testResultFile)
 }
--- unitTests/NRSA_Physical_Habitat/testSuiteNRSAMetrics.R
+++ unitTests/NRSA_Physical_Habitat/testSuiteNRSAMetrics.R
@@ -3,13 +3,18 @@
 # 10/21/15 cws Changed to use nrsa*.r and aquametPathList
 #  2/20/24 cws Added rngKind and rngNormalKind values to correct defaults as was
 #          recently done for other test suites.
+#  2/21/24 cws Specifying full path for output file
 #
 
 testSuiteNRSAMetrics <- function()
 # Define and run the test suite for the NRSA physical habitat metrics functions
 # Results are saved to timestamped HTML file
 {
-  testSuite <- defineTestSuite("NRSA metrics"
+  testsuiteTitle <- sprintf('Aquamet NRSA metrics in %s and %s'
+                           ,ifelse(Sys.getenv('RSTUDIO') == '', 'RGui', 'RStudio')
+                           ,R.version.string
+                           )
+  testSuite <- defineTestSuite(testsuiteTitle
                               ,dirs=aquametPathList                     # defined in aquamet.r
                               ,testFileRegexp="^nrsa.*\\.r$"
                               ,testFuncRegexp="^.+Test$"
@@ -19,6 +24,6 @@ testSuiteNRSAMetrics <- function()
 
   testResult <- runTestSuite(testSuite)
 
-  testResultFile <- "testResults_NRSAMetrics.html"
+  testResultFile <- paste0(mainPath, "/testResults_NRSAMetrics.html")
   printHTMLProtocol(testResult, fileName=testResultFile)
 }
